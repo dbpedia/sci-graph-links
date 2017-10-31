dc54e490743a9ff27ec8e949e9ed6c7c32d9649c
Beyza <beyza_yaman@hotmail.com>
2017-10-31 17:23:19 +0100

    updated bash file for abstracts

--- NER/data-samples/bashAbstract.sh
+++ NER/data-samples/bashAbstract.sh
@@ -9,9 +9,9 @@ do
 
 echo "$f"
 filename=$(basename "$f" .txt) 
+temp=$(cat $f)
 echo $filename
-curl -X POST http://api.dbpedia-spotlight.org/annotate --data @"$f"  --data "prefix=$PREFIX" --data "confidence=0.35" -H "Accept:text/turtle" -o $filename.nt 
-
+curl -X POST http://api.dbpedia-spotlight.org/annotate --data "text=$temp" --data "prefix=$PREFIX" --data "confidence=0.35"  --data "support=20" -H "Accept:text/turtle" -o $filename.nt 
 done 
 
 echo "FINISHED"
